7d7efa77bdbf8bc3b886638f7303ceed1f6c6900
Dikshant23529 <latherdikshant0@gmail.com>
2024-08-30 00:31:50 +0530

    tech blog project started

---
+++ src/main/java/blog/tech/TechblogApplication.java
@@ -0,0 +1,13 @@
+package blog.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechblogApplication.class, args);
+	}
+
+}
